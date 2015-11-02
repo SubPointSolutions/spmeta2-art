98cf097e94d0ca90f4f36e7d7cdae9f56ff9518e
support@subpointsolutions.com <support@subpointsolutions.com>
2015-11-02 23:42:25 +1100

    p1

---
+++ M2.ArtPortal/M2.ArtPortal.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace M2.ArtPortal.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
